74681ed7940525d66c6fe83a4de1930eeb8833d2
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2020-06-12 19:05:23 -0300

    :bug: Fix error handling in IOCTL calls

--- src/ioctl.jl
+++ src/ioctl.jl
@@ -16,12 +16,12 @@ end
 
 function _ioctl(fd::Integer, request::Integer, arg::Base.RefValue{T}) where T
     ret = ccall(:ioctl, Cint, (Cint, Culong, Ref{T}), fd, request, arg)
-    ret < 0 && throw(SystemError("Error in IOCTL call. Errno: $(Libc.errno())."))
+    ret < 0 && throw(SystemError("Error in IOCTL call", Libc.errno()))
     return ret
 end
 
 function _ioctl(fd::Integer, request::Integer, arg::AbstractVector{T}) where T
     ret = ccall(:ioctl, Cint, (Cint, Culong, Ref{T}), fd, request, arg)
-    ret < 0 && throw(SystemError("Error in IOCTL call. Errno: $(Libc.errno())."))
+    ret < 0 && throw(SystemError("Error in IOCTL call", Libc.errno()))
     return ret
 end
