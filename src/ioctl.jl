a297bcf8289756c1fa3e68f32274135672092b94
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-03-31 22:28:03 -0300

    :bug: Replace ccalls due to Julia v1.7 compatibility

--- src/ioctl.jl
+++ src/ioctl.jl
@@ -9,20 +9,33 @@
 
 @inline _ioctl(io::IOStream, args...) = _ioctl(fd(io), args...)
 
-function _ioctl(fd::Integer, request::Integer, arg::T) where T <: Number
-    ret = ccall(:ioctl, Cint, (Cint, Culong, T), fd, request, arg)
-    ret < 0 && throw(SystemError("Error in IOCTL call", Libc.errno()))
-    return ret
-end
+for (Tj, Tc) in (
+    (Int8, Cchar),
+    (Int16, Cshort),
+    (Int32, Cint),
+    (Int64, Clonglong),
+    (UInt8, Cuchar),
+    (UInt16, Cushort),
+    (UInt32, Cuint),
+    (UInt64, Culonglong),
+    (Float32, Cfloat),
+    (Float64, Cdouble)
+)
+    @eval function _ioctl(fd::Integer, request::Integer, arg::$Tj)
+        ret = ccall(:ioctl, Cint, (Cint, Culong, $Tc), fd, request, arg)
+        ret < 0 && throw(SystemError("Error in IOCTL call", Libc.errno()))
+        return ret
+    end
 
-function _ioctl(fd::Integer, request::Integer, arg::Base.RefValue{T}) where T
-    ret = ccall(:ioctl, Cint, (Cint, Culong, Ref{T}), fd, request, arg)
-    ret < 0 && throw(SystemError("Error in IOCTL call", Libc.errno()))
-    return ret
-end
+    @eval function _ioctl(fd::Integer, request::Integer, arg::Base.RefValue{$Tj})
+        ret = ccall(:ioctl, Cint, (Cint, Culong, Ref{$Tc}), fd, request, arg)
+        ret < 0 && throw(SystemError("Error in IOCTL call", Libc.errno()))
+        return ret
+    end
 
-function _ioctl(fd::Integer, request::Integer, arg::AbstractVector{T}) where T
-    ret = ccall(:ioctl, Cint, (Cint, Culong, Ref{T}), fd, request, arg)
-    ret < 0 && throw(SystemError("Error in IOCTL call", Libc.errno()))
-    return ret
+    @eval function _ioctl(fd::Integer, request::Integer, arg::AbstractVector{$Tj})
+        ret = ccall(:ioctl, Cint, (Cint, Culong, Ref{$Tc}), fd, request, arg)
+        ret < 0 && throw(SystemError("Error in IOCTL call", Libc.errno()))
+        return ret
+    end
 end
